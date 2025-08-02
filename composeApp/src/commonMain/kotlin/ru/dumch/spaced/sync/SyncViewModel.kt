d86fc8de8bd2d00438a820b328bd10f6863c7ba3
dumch <arturdumchev@yandex.ru>
2025-08-02 20:55:58 +0300

    Extract base viewModel

--- composeApp/src/commonMain/kotlin/ru/dumch/spaced/sync/SyncViewModel.kt
+++ composeApp/src/commonMain/kotlin/ru/dumch/spaced/sync/SyncViewModel.kt
@@ -1,6 +1,5 @@
 package ru.dumch.spaced.sync
 
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.appstractive.dnssd.DiscoveredService
 import com.appstractive.dnssd.DiscoveryEvent
@@ -10,26 +9,26 @@ import com.appstractive.dnssd.key
 import com.diamondedge.logging.logging
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
-import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import org.kodein.di.DI
 import org.kodein.di.DIAware
 import org.kodein.di.instance
 import ru.dumch.spaced.sync.SyncCommon.SERVICE_TYPE
-import ru.dumch.spaced.ui.ViewModelWithUtils
+import ru.dumch.spaced.ui.Event
+import ru.dumch.spaced.ui.SideEffect
+import ru.dumch.spaced.ui.State
+import ru.dumch.spaced.ui.BaseViewModel
 import java.util.concurrent.ConcurrentHashMap
-import kotlin.collections.set
 
-data class SyncState(
+internal data class SyncState(
     val registered: Boolean = false,
     val registerInProgress: Boolean = false,
     val tabIdx: Int = 0,
-
     val isScanning: Boolean = false,
     val scannedServices: List<DiscoveredService> = emptyList(),
-)
+) : State
 
-internal sealed interface SyncEvent {
+internal sealed interface SyncEvent : Event {
     data class TabSelected(val i: Int) : SyncEvent
     object RegisterOn : SyncEvent
     object RegisterOff : SyncEvent
@@ -37,73 +36,43 @@ internal sealed interface SyncEvent {
     object ScanOff : SyncEvent
 }
 
-internal sealed interface SyncSideEffect {
+internal sealed interface SyncSideEffect : SideEffect {
     object RegisterConnected : SyncSideEffect
     object RegisterDisconnected : SyncSideEffect
     data class ServiceDiscovered(val event: DiscoveryEvent) : SyncSideEffect
 }
 
-internal class SyncViewModel(override val di: DI) : ViewModel(), DIAware {
+internal class SyncViewModel(override val di: DI) : 
+    BaseViewModel<SyncState, SyncEvent, SyncSideEffect>(), DIAware {
+    
     private val l = logging(tag = SyncViewModel::class.simpleName)
     private val netService: NetService by di.instance()
-    private val _uiState: MutableStateFlow<SyncState> = MutableStateFlow(SyncState())
-    private val _events = MutableSharedFlow<SyncEvent>()
-    private val _effects = MutableSharedFlow<SyncSideEffect>()
-
-    private val currentState: SyncState get() = uiState.value
-    val uiState: StateFlow<SyncState> = _uiState
-    val effects: SharedFlow<SyncSideEffect> = _effects
-
     private var scanJob: Job? = null
     private var discoveredServices = ConcurrentHashMap<String, DiscoveredService>()
 
+    override fun initialState() = SyncState()
+
     init {
         l.info { "init" }
-        viewModelScope.launch { _events.collect(::handleEvent) }
-        viewModelScope.launch { _effects.collect(::handleSideEffect) }
+        init()
         viewModelScope.launch {
             netService.isRegistered.collect { isRegistered ->
                 val eff = if (isRegistered) SyncSideEffect.RegisterConnected else SyncSideEffect.RegisterDisconnected
-                _effects.emit(eff)
+                send(eff)
             }
         }
     }
 
-    fun send(event: SyncEvent) = viewModelScope.launch { _events.emit(event) }
-
-    suspend fun setState(reduce: SyncState.() -> SyncState) {
-        val newState = currentState.reduce()
-        _uiState.emit(newState)
-    }
-
-    private suspend fun handleSideEffect(effect: SyncSideEffect) {
+    override suspend fun handleSideEffect(effect: SyncSideEffect) {
         l.info { "handleSideEffect: $effect" }
         when (effect) {
             is SyncSideEffect.RegisterConnected -> setState { copy(registered = true, registerInProgress = false) }
-            SyncSideEffect.RegisterDisconnected -> setState { copy(registered = false, registerInProgress = false) }
-            is SyncSideEffect.ServiceDiscovered -> onServiceDiscovered(effect)
+            is SyncSideEffect.RegisterDisconnected -> setState { copy(registered = false, registerInProgress = false) }
+            is SyncSideEffect.ServiceDiscovered -> onServiceDiscovered(SyncSideEffect.ServiceDiscovered(effect.event))
         }
     }
 
-    private suspend fun onServiceDiscovered(effect: SyncSideEffect.ServiceDiscovered) {
-        val discoveryEvent = effect.event
-        when (discoveryEvent) {
-            is DiscoveryEvent.Discovered -> {
-                discoveryEvent.resolve()
-            }
-
-            is DiscoveryEvent.Removed -> {
-                discoveredServices.remove(discoveryEvent.service.key)
-            }
-
-            is DiscoveryEvent.Resolved -> {
-                discoveredServices[discoveryEvent.service.key] = discoveryEvent.service
-            }
-        }
-        setState { copy(scannedServices = discoveredServices.values.toList()) }
-    }
-
-    private suspend fun handleEvent(event: SyncEvent) {
+    override suspend fun handleEvent(event: SyncEvent) {
         l.info { "handleEvent: $event" }
         when (event) {
             is SyncEvent.TabSelected -> setState { copy(tabIdx = event.i) }
@@ -127,11 +96,29 @@ internal class SyncViewModel(override val di: DI) : ViewModel(), DIAware {
                 discoveredServices.clear()
                 scanJob = viewModelScope.launch(Dispatchers.IO) {
                     discoverServices(SERVICE_TYPE).collect { discoveryEvent: DiscoveryEvent ->
-                        _effects.emit(SyncSideEffect.ServiceDiscovered(discoveryEvent))
+                        send(SyncSideEffect.ServiceDiscovered(discoveryEvent))
                     }
                 }
                 setState { copy(isScanning = true) }
             }
         }
+    }
+
+    private suspend fun onServiceDiscovered(effect: SyncSideEffect.ServiceDiscovered) {
+        val discoveryEvent = effect.event
+        when (discoveryEvent) {
+            is DiscoveryEvent.Discovered -> {
+                discoveryEvent.resolve()
+            }
+
+            is DiscoveryEvent.Removed -> {
+                discoveredServices.remove(discoveryEvent.service.key)
+            }
+
+            is DiscoveryEvent.Resolved -> {
+                discoveredServices[discoveryEvent.service.key] = discoveryEvent.service
+            }
+        }
+        setState { copy(scannedServices = discoveredServices.values.toList()) }
     }
 }
